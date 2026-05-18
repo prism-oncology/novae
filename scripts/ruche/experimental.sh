599f7cca5d645ff2342f77e4cc19bf43bcc312dc
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2026-05-18 19:39:40 +0200

    use cpu long

--- scripts/ruche/experimental.sh
+++ scripts/ruche/experimental.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=novae
 #SBATCH --output=/gpfs/workdir/blampeyq/.jobs_outputs/%j
 #SBATCH --time=10:00:00
-#SBATCH --partition=cpu_short
+#SBATCH --partition=cpu_long
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=8
 
