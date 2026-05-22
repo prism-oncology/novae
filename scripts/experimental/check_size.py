22f632bbc52dc00651cd0a050d43a01d9947b7a2
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2026-05-22 09:32:03 +0200

    minimal stats

--- scripts/experimental/check_size.py
+++ scripts/experimental/check_size.py
@@ -6,18 +6,30 @@ from pathlib import Path
 
 import scanpy as sc
 
+other_dir_names = [f"X_scConcept{1 + i}" for i in range(4)]
+
+
+def _print_stats(adata: sc.AnnData, name: str) -> None:
+    X = adata.obsm["X_scConcept"]
+    print(f"{name}\nshape{X.shape}, mean:{X.mean(0)}, std:{X.std(0)}\n\n")
+
 
 def main(directory: Path) -> None:
     paths = list(directory.glob("*.h5ad"))
 
     for path in paths:
-        name = path.stem
         adata = sc.read_h5ad(path)
 
-        print(name, adata.obsm["X_scConcept"].shape)
+        _print_stats(adata, "default")
+
+        for other_dir_name in other_dir_names:
+            other_path = directory.parent / other_dir_name / path.name
+            if not other_path.exists():
+                print(f"Not existing for {other_dir_name}.")
+                continue
+
+            _print_stats(sc.read_h5ad(other_path), other_dir_name)
 
 
 if __name__ == "__main__":
-    for directory in Path("/gpfs/workdir/blampeyq/res_novae").glob("X_scConcept*"):
-        print(directory)
-        main(directory)
+    main(Path("/gpfs/workdir/blampeyq/res_novae/X_scConcept"))
